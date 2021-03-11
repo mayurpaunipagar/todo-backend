253ad7ab2f4af8593a7e77e702adb9a644aed035
MayurHpNotebook <001mayur94@gmail.com>
2021-03-11 13:55:52 +0530

    added db connection

--- src/server.js
+++ src/server.js
@@ -1,8 +1,28 @@
 const express=require('express');
+const mongoose=require('mongoose');
+const cors=require('cors');
+const bcrypt=require('bcrypt');
+const session=require('express-session');
+
+const session_secret="todolist";
+
 const app=express();
+app.use(express.json());
+app.use(cors({
+    credentials:true,
+    origin:"*"
+}))
+
+app.use(session({
+    secret:session_secret,
+    cookie:{maxAge:1*60*60*1000}
+}));
+
+//connect with mongodb
+const db=mongoose.createConnection("mongodb+srv://user:userpassword@meditation-community-ap.vgg6t.mongodb.net/todoApp?retryWrites=true&w=majority");
 app.get('/',(req,res)=>{
     res.send("Welcome to mayur's todo app");
 })
-app.listen(9999,()=>{
-    console.log("listening @ 9999");
+app.listen(process.env.PORT,()=>{
+    console.log("listening @",process.env.PORT);
 })
